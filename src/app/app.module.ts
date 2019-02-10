d74e5e507e700b33d84126482ea3d9a779854039
dezGusty <thegusty999@gmail.com>
2019-02-11 00:05:07 +0200

    added bootstrap + some components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CoreComponent } from './core/core.component';
+import { HeaderComponent } from './core/header/header.component';
+import { MatchesComponent } from './matches/matches.component';
+import { NextMatchComponent } from './matches/next-match/next-match.component';
+import { PlayersComponent } from './players/players.component';
+import { PlayerComponent } from './players/player/player.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CoreComponent,
+    HeaderComponent,
+    MatchesComponent,
+    NextMatchComponent,
+    PlayersComponent,
+    PlayerComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/matches/next-match/next-match.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-next-match',
+  templateUrl: './next-match.component.html',
+  styleUrls: ['./next-match.component.css']
+})
+export class NextMatchComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
