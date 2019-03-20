58d21c60da4506fce69ea5315bdcaea3056d9605
dezGusty <thegusty999@gmail.com>
2019-03-20 23:57:20 +0200

    play around with the styles a bit

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -11,7 +11,7 @@ export class AboutComponent implements OnInit {
   public releaseDate: Date;
 
   constructor() {
-    this.version = '0.4.1';
+    this.version = '0.4.2';
     this.releaseDate = new Date(2019, 3, 20);
   }
 
