8a4d65e4587d4ac91b7a39ce8c90c610b96e3294
dezGusty <thegusty999@gmail.com>
2020-03-28 20:24:57 +0200

    add insertion of content in SVG

--- bin/git-branch-to-favicon.js
+++ bin/git-branch-to-favicon.js
@@ -10,26 +10,27 @@ hash.update(branchName);
 const color = '#' + hash.digest().toString('hex').substring(0, 6);
 const invertedColor = invertColor(color, true);
 
-function getSVGStandaloneContent(branchName, invertedColor) {
+function getSVGStandaloneContent(branchName, nonTransparentBackColor, invertedColor) {
   console.log('getSVGStandaloneContent', branchName, invertedColor);
-  
+
   return `<svg viewBox="0 0 64 64" font-size="32" font-family="monospace,monospace" xmlns="http://www.w3.org/2000/svg">
-  <rect y="6" width="64" height="32" fill="${color}"/>
+  <rect y="6" width="64" height="32" fill="${nonTransparentBackColor}"/>
   <text y="32" textLength="64" lengthAdjust="spacingAndGlyphs"
     fill="${invertedColor}">${branchName.substring(0, 4)}</text>
 </svg>`;
 }
 
-function getSVGSubgroupContent(branchName, invertedColor) {
+function getSVGSubgroupContent(branchName, nonTransparentBackColor, invertedColor) {
+  const alphaColor = nonTransparentBackColor + 'A7';
   return `<g>
-  <rect y="6" width="64" height="32" fill="${color}"/>
-  <text y="32" textLength="64" lengthAdjust="spacingAndGlyphs"
+  <rect y="20" width="400" height="160" fill="${alphaColor}"/>
+  <text y="128" font-size="128" font-family="monospace,monospace" textLength="256" lengthAdjust="spacingAndGlyphs"
     fill="${invertedColor}">${branchName.substring(0, 4)}</text>
 </g>`;
 }
 
 module.exports = {
-  getFullSVG: () => { return getSVGStandaloneContent(branchName, invertedColor); },
-  getSubSVG: () => { return getSVGSubgroupContent(branchName, invertedColor); },
+  getFullSVG: () => { return getSVGStandaloneContent(branchName, color, invertedColor); },
+  getSubSVG: () => { return getSVGSubgroupContent(branchName, color, invertedColor); },
   branchName
 };
--- bin/icon-updater.js
+++ bin/icon-updater.js
@@ -1,9 +1,41 @@
 let fs = require('fs');
 let gitsvg = require('./git-branch-to-favicon.js');
 const targetFile = 'src/favicon.svg';
-fs.writeFile(targetFile, gitsvg.getFullSVG(), function (err) {
-    if (err) {
-        throw err;
+
+const useStandalone = false;
+if (useStandalone) {
+    fs.writeFile(targetFile, gitsvg.getFullSVG(), function (err) {
+        if (err) {
+            throw err;
+        }
+        console.log(`Saved [${gitsvg.branchName}] branch icon to [${targetFile}]!`);
+    });
+} else {
+    if (mergeSVGs(
+        'src/favicon.prod.svg',
+        gitsvg.getSubSVG(),
+        targetFile)) {
+        console.log(`Saved [${gitsvg.branchName}] branch icon to [${targetFile}]!`);
+    };
+}
+
+
+function mergeSVGs(fullSrcFileName, addonContent, outputFileName) {
+    try {
+        let rawSVG = fs.readFileSync(fullSrcFileName).toString();
+
+        let combinedContent = '';
+
+        rawSVG.split('\n').forEach(element => {
+            if (element.trim() === '</svg>') {
+                combinedContent += addonContent;
+            }
+            combinedContent += element + '\n';
+        });
+        fs.writeFileSync(outputFileName, combinedContent);
+
+        return true;
+    } catch (error) {
+        return false;
     }
-    console.log(`Saved [${gitsvg.branchName}] branch icon to [${targetFile}]!`);
-}); 
+}
