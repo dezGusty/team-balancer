d6364a9833a1d3ea4b00030c7bcf84d6a40f41dc
Augustin Preda <thegusty999@gmail.com>
2022-03-01 13:12:41 +0200

    version bump

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -20,7 +20,7 @@ export class AboutComponent implements OnInit {
     this.author = 'Gusti';
     this.version = pkg.version;
 
-    this.releaseDate = new Date('2022-02-26');
+    this.releaseDate = new Date('2022-03-01');
   }
 
   ngOnInit() {
