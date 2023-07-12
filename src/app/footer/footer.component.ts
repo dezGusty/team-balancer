678ab64b69eff6107570aa296c852e6a5e3fb163
Augustin Preda <thegusty999@gmail.com>
2023-07-12 19:44:53 +0300

    minor clean-up

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,9 +1,9 @@
-import { CommonModule } from '@angular/common';
+import { CommonModule, DatePipe } from '@angular/common';
 import { Component } from '@angular/core';
 import pkg from '../../../package.json';
 
 @Component({
-  imports: [CommonModule],
+  imports: [CommonModule, DatePipe],
   selector: 'app-footer',
   standalone: true,
   styles: [''],
@@ -14,7 +14,6 @@ export class FooterComponent {
   public releaseDate: Date;
   public author: string;
 
-  private topics: string[] = [];
   constructor(
 
   ) {
